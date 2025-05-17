091bcb39fc3d2d14ef8d3584684921c0a6d3a722
ampahben <benjaminkwame44@gmail.com>
2025-05-17 21:20:12 +0000

    added new apps

---
+++ backend/users/models.py
@@ -0,0 +1,10 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class CustomUser(AbstractUser):
+#     # Add custom fields here
+#     phone_number = models.CharField(max_length=20, blank=True)
+    
+#     class Meta:
+#         verbose_name = 'Custom User'
+#         verbose_name_plural = 'Custom Users'
